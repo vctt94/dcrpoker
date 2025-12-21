cd4ff0797a86f4b705f085a5a3d788be98970f7f
unknown <victor.duarte.oliveira@gmail.com>
2025-12-21 13:02:15 -0300

    remove unecessary cmd

--- pokerui/golibbuilder/windows_amd64.go
+++ pokerui/golibbuilder/windows_amd64.go
@@ -3,13 +3,10 @@
 
 package golibbuilder
 
-// 1) Build the DLL
+// Build the DLL
 //go:generate go build -buildmode=c-shared -o ../build/windows/amd64/golib.dll ../golib/sharedlib
 
-// 2) Copy to the Flutter Windows plugin (adjust path if yours differs)
-//go:generate powershell -NoProfile -Command "New-Item -ItemType Directory -Force ../flutterui/plugin/windows | Out-Null; Copy-Item -Force ../build/windows/amd64/golib.dll ../flutterui/plugin/windows/golib.dll"
-
-// 3) Copy into the built runner output (Release) so your .exe finds it next to the exe
+//  Copy into the built runner output (Release) so your .exe finds it next to the exe
 //    This matches: build\windows\x64\runner\Release\dcrpoker.exe
 //go:generate powershell -NoProfile -Command "New-Item -ItemType Directory -Force ../flutterui/pokerui/build/windows/x64/runner/Release | Out-Null; Copy-Item -Force ../build/windows/amd64/golib.dll ../flutterui/pokerui/build/windows/x64/runner/Release/golib.dll"
 
